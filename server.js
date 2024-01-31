359be537c6b2fad73b49543ab0da21d7bc5225d4
Jordan Wofford <padrejulian723@gmail.com>
2024-01-31 17:40:54 -0500

    folderSetup and server js config:

---
+++ server.js
@@ -0,0 +1,30 @@
+// ----------------------------[Requires]
+require('dotenv').config()
+require('./config/database');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const port = 3001;
+const app = express();
+// const ensureLoggedIn = require('./config/ensureLoggedIn');
+
+// ----------------------------[Middleware]
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+app.use(require('./config/checkToken'));
+app.use('/api/users', require('./routes/api/users'));
+
+
+
+// ----------------------------[Routes]
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// ----------------------------[Server]
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
